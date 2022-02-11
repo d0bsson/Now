ef490af61b55379aa0a20d6b4b59094b09e05a8d
David <ber.dev@yandex.ru>
2022-02-11 21:34:06 +0300

    test commit

---
+++ Now/BarAndCultureViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Now
+//
+//  Created by Дэвид Бердников on 09.02.2022.
+//
+
+import UIKit
+
+class BarAndCultureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
